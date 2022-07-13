4d5d820d7dcbeba973872ab62fe1d5060fdd43f4
Pavel Taranov <pavel.a.taranov@gmail.com>
2022-07-13 21:01:07 +0300

    Set of performance tests for mysql

---
+++ lib/tsrc/PerformanceTest/test.sh
@@ -0,0 +1,11 @@
+mocha --timeout=10000000 ./perormance.mysql.memory.pool.1.1000.js  > "./perormance.mysql.memory.pool.1.1000.$(date).log"
+mocha --timeout=10000000 ./perormance.mysql.memory.pool.16.1000.js > "./perormance.mysql.memory.pool.16.1000.$(date).log"
+mocha --timeout=10000000 ./perormance.mysql.memory.pool.64.1000.js > "./perormance.mysql.memory.pool.64.1000.$(date).log"
+
+mocha --timeout=10000000 ./perormance.mysql.innodb.no.indexses.pool.1.1000.js  > "perormance.mysql.innodb.no.indexses.pool.1.1000.$(date).log"
+mocha --timeout=10000000 ./perormance.mysql.innodb.no.indexses.pool.16.1000.js > "perormance.mysql.innodb.no.indexses.pool.16.1000.$(date).log"
+mocha --timeout=10000000 ./perormance.mysql.innodb.no.indexses.pool.64.1000.js > "perormance.mysql.innodb.no.indexses.pool.64.1000.$(date).log"
+
+mocha --timeout=10000000 ./perormance.mysql.innodb.pool.1.1000.js  > "perormance.mysql.innodb.pool.1.1000.$(date).log"
+mocha --timeout=10000000 ./perormance.mysql.innodb.pool.16.1000.js > "perormance.mysql.innodb.pool.16.1000.$(date).log"
+mocha --timeout=10000000 ./perormance.mysql.innodb.pool.64.1000.js > "perormance.mysql.innodb.pool.64.1000.$(date).log"
